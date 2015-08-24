b55c4e5109d7b19f7dbd0820a96b5231953d6821
Jonathan Eunice <jonathan.eunice@gmail.com>
2015-08-23 21:22:19 -0400

    reorg

--- quoter/__init__.py
+++ quoter/__init__.py
@@ -1,4 +1,5 @@
 from .quoter import *
+from .xmlhtml import *
 from .joiner import join, word_join, and_join, or_join, concat, joinlines, items
 from .util import is_string, stringify, blanknone
 from .version import __version__

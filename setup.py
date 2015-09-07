fb62be63b159ce26e23c75c807e9fa4a708ff69a
Jonathan Eunice <jonathan.eunice@gmail.com>
2015-09-07 17:28:59 -0400

    1.6.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def lines(text):
 
 setup(
     name='quoter',
-    version='1.6.1',
+    version='1.6.2',
     author='Jonathan Eunice',
     author_email='jonathan.eunice@gmail.com',
     description="Powerful way to construct text, HTML, and XML, plus a kick-ass join",
